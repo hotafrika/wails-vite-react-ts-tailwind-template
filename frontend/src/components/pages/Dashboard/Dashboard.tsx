c32bd87452308dabaff37f57ae52b17b9d3705a3
Rentoo <dreamYiZ@outlook.com>
2022-11-07 05:32:43 +0800

    -

--- frontend/src/components/pages/Dashboard/Dashboard.tsx
+++ frontend/src/components/pages/Dashboard/Dashboard.tsx
@@ -13,11 +13,11 @@ function Dashboard() {
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
         </div>
-        <div>
+        <div className="mt-4">
           <div>
             <Stats />
           </div>
-          <div>
+          <div className="mt-4">
             <div><Practice /></div>
             <div><Playlist /></div>
           </div>
