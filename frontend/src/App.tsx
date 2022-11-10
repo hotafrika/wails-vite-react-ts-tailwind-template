22b1db54d460a47697a1dffeb5b33a57369b8225
kinghoddy <odunmilade@gmail.com>
2022-11-10 20:45:34 +0100

    init commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,6 +17,7 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 import Sidebar from "./components/layout/Sidebar";
 import HeaderFootwCard from "./components/layout/HeaderFootwCard";
+import MusicPlayer from "./components/layout/MusicPlayer";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -40,7 +41,7 @@ function App() {
               <Bars3Icon className="h-6 w-6" aria-hidden="true" />
             </button>
           </div>
-          <main className="flex-1">
+          <main className="flex-1 relative">
             <div className="py-6">
               <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
                 <HeaderFootwCard>
@@ -63,6 +64,9 @@ function App() {
                                 </div> */}
                 {/* /End replace */}
               </div>
+            </div>
+            <div className="bg-[#f90] fixed bottom-0 left-[16] w-[100%] ">
+              <MusicPlayer />
             </div>
           </main>
         </div>
